7e4cf2abfbc5c477982c17397e0ea52dfa3313d6
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-10-27 15:58:40 +0530

    chore: Update interfaces

--- src/interfaces/IHooks.sol
+++ src/interfaces/IHooks.sol
@@ -4,7 +4,23 @@ pragma solidity 0.8.24;
 interface IHooks {
     function afterStreamCreated(bytes32 _streamHash) external;
 
-    function beforeFundsCollected(bytes32 _streamHash) external;
+    function beforeFundsCollected(bytes32 _streamHash, uint256 _amount) external;
 
-    function afterFundsCollected(bytes32 _streamHash) external;
+    function afterFundsCollected(bytes32 _streamHash, uint256 _amount) external;
+
+    function beforeStreamUpdated(bytes32 _streamHash) external;
+
+    function afterStreamUpdated(bytes32 _streamHash) external;
+
+    function beforeStreamClosed(bytes32 _streamHash) external;
+
+    function afterStreamClosed(bytes32 _streamHash) external;
+
+    function beforeStreamPaused(bytes32 _streamHash) external;
+
+    function afterStreamPaused(bytes32 _streamHash) external;
+
+    function beforeStreamUnPaused(bytes32 _streamHash) external;
+
+    function afterStreamUnPaused(bytes32 _streamHash) external;
 }
