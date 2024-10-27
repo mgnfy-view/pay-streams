194895502c338c633e224b77e5e45c7f21be6fe2
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-10-27 21:19:34 +0530

    refactor: Add a fee amount field to the hooks called before and after funds are collected

--- src/interfaces/IHooks.sol
+++ src/interfaces/IHooks.sol
@@ -4,9 +4,9 @@ pragma solidity 0.8.24;
 interface IHooks {
     function afterStreamCreated(bytes32 _streamHash) external;
 
-    function beforeFundsCollected(bytes32 _streamHash, uint256 _amount) external;
+    function beforeFundsCollected(bytes32 _streamHash, uint256 _amount, uint256 _feeAmount) external;
 
-    function afterFundsCollected(bytes32 _streamHash, uint256 _amount) external;
+    function afterFundsCollected(bytes32 _streamHash, uint256 _amount, uint256 _feeAmount) external;
 
     function beforeStreamUpdated(bytes32 _streamHash) external;
 
