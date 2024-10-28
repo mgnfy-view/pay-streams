77b42c7aa7cfe4b4e87c7c16ee2678df9d5c4091
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-10-28 11:02:45 +0530

    chore: Whitelist PYUSD for streaming on deployment script

--- script/DeployPayStreams.s.sol
+++ script/DeployPayStreams.s.sol
@@ -7,10 +7,12 @@ import { PayStreams } from "../src/PayStreams.sol";
 
 contract DeployPayStreams is Script {
     uint16 feeInBasisPoints = 10;
+    address public constant PYUSD = 0xCaC524BcA292aaade2DF8A05cC58F0a65B1B3bB9;
 
     function run() external returns (address) {
         vm.startBroadcast();
         PayStreams stream = new PayStreams(feeInBasisPoints);
+        stream.setToken(PYUSD, true);
         vm.stopBroadcast();
 
         return address(stream);
