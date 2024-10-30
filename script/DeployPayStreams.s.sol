a885457f40d899fba581125b7e2f6741dccfb2b9
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-10-30 12:04:30 +0530

    refactor: Initialize variables inside the run() function

--- script/DeployPayStreams.s.sol
+++ script/DeployPayStreams.s.sol
@@ -6,13 +6,16 @@ import { Script } from "forge-std/Script.sol";
 import { PayStreams } from "../src/PayStreams.sol";
 
 contract DeployPayStreams is Script {
-    uint16 feeInBasisPoints = 10;
-    address public constant PYUSD = 0xCaC524BcA292aaade2DF8A05cC58F0a65B1B3bB9;
+    uint16 public feeInBasisPoints;
+    address public pyusd;
 
     function run() external returns (address) {
+        feeInBasisPoints = 10;
+        pyusd = 0xCaC524BcA292aaade2DF8A05cC58F0a65B1B3bB9;
+
         vm.startBroadcast();
         PayStreams stream = new PayStreams(feeInBasisPoints);
-        stream.setToken(PYUSD, true);
+        stream.setToken(pyusd, true);
         vm.stopBroadcast();
 
         return address(stream);
